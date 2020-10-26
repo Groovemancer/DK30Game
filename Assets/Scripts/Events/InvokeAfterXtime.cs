8f6ac2695ca6c3100b8d000ecb78fa5b883fea29
Eric Fallone <ericfallone@gmail.com>
2020-10-25 23:55:26 -0400

    Invoke after x amount of time of being enabled
    Invoke unity events after a start delay

---
+++ Assets/Scripts/Events/InvokeAfterXtime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeAfterXtime : MonoBehaviour
+{
+	public UnityEvent ActivationEvents;
+
+	public float startDelay = 0f;
+
+	void Start()
+	{
+		Invoke("InvokeEvents", startDelay);
+	}
+
+	public void InvokeEvents()
+	{
+		ActivationEvents.Invoke();
+	}
+}
--- Assets/Scripts/Events/InvokeRepeating.cs
+++ Assets/Scripts/Events/InvokeRepeating.cs
@@ -9,7 +9,7 @@ public class InvokeRepeating : MonoBehaviour
 	public UnityEvent FirstActivationEvents;
 	public UnityEvent SecondActivationEvents;
 
-	public float delayTime;
+	public float delayBetweenCalls;
 
 	public float startDelay = 0f;
 	public float delayBefore2ndEvent = 1f; 
@@ -17,7 +17,7 @@ public class InvokeRepeating : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		InvokeRepeating("FirstEvents", startDelay, delayTime);
+		InvokeRepeating("FirstEvents", startDelay, delayBetweenCalls);
 	}
 
 	private void FirstEvents()
