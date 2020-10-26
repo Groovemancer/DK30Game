2bea547c027694dd70aed7ab83ede0cd07a21336
Eric Fallone <ericfallone@gmail.com>
2020-10-26 15:30:58 -0400

    Set up example scene and commented look at game object

--- Assets/Scripts/ToActivate/LookAtGameObject.cs
+++ Assets/Scripts/ToActivate/LookAtGameObject.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class LookAtGameObject : MonoBehaviour
 {
 	public GameObject Target;
 
+	//replace update method with the following
+
+	//public void TeleportTarget()
+	//{
+	//	StartCoroutine(Teleport())
+	//}
+
+	//IENorator Teleport()
+	//{
+		// teleporter logic here
+	//}
+
+	//remove this update method
 	private void Update()
 	{
 		if (Target != null)
@@ -16,6 +32,9 @@ public class LookAtGameObject : MonoBehaviour
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		}
 	}
+
+	//Two different set targets to be flexible later, however, it just uses the first one at the moment
+	//By passing in the unity event target helper you can get the gameobject of the triggering charater. 
 
 	public void SetTarget(UnityEventTargetPasserHelper TargetInHelper)
 	{
