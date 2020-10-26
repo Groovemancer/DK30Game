f7c6f46ed49dcf06ff51b0cfd1fdb2e3a9e21fae
westfash <westfash@gmail.com>
2020-10-25 20:35:19 -0500

    Teleporter Prefab and Lighting Update
    - Made a Teleporter prefab that acts like the other base activatables
    - The teleporter that move the player when that activate it
    - Two sprite lights appear at start and destination
    - Updated script
    - Added shadow caster to player prefab
    - lights not cast shadows

--- Assets/Scripts/Activatables/Teleporter.cs
+++ Assets/Scripts/Activatables/Teleporter.cs
@@ -1,34 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teleporter : MonoBehaviour
+public class Teleporter : BaseActivatable
 {
     // Start is called before the first frame update
     public Transform Destination;
     public GameObject Player;
+    //public Light Teleporter_Light;
     public int delayToDeActivate;
 
-
+    //
+    void Start()
+    {
+      
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (isActive == false && Time.time - LastActivated > DoubleTapPreventionDelay && Input.GetButtonDown("Activate"))
+        {
+            // Debug.Log("The button was pressed");
+            LastActivated = Time.time;
+            StartCoroutine(TeleportPlayer());
+            
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-
-            StartCoroutine(TeleportPlayer());
-        }
+        // Debug.Log("The area was entered");
+         if (other.gameObject.tag == "Player")
+         {
+            Player = other.gameObject;
+         }
     }
 
     IEnumerator TeleportPlayer()
     {
-        
-        yield return new WaitForSeconds(1);
+        //Teleporter_Light.intensity = .5f;
+        isActive = true;
+        InvokeOnActive();
+        yield return new WaitForSeconds(delayToDeActivate);
         Player.transform.position = new Vector2(Destination.transform.position.x, Destination.transform.position.y);
+        isActive = false;
+        InvokeOnDeActive();
+        //Teleporter_Light.intensity = 0f;
     }
 }
