bf875ed1d5b5f0ee0591429025cce3ee10f48ce4
Eric Fallone <ericfallone@gmail.com>
2020-10-25 23:30:30 -0400

    Events repeating after x amount of seconds
    Added a component that calls unity events repeatily over time.

---
+++ Assets/Scripts/Events/InvokeRepeating.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeRepeating : MonoBehaviour
+{
+
+	public UnityEvent FirstActivationEvents;
+	public UnityEvent SecondActivationEvents;
+
+	public float delayTime;
+
+	public float startDelay = 0f;
+	public float delayBefore2ndEvent = 1f; 
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		InvokeRepeating("FirstEvents", startDelay, delayTime);
+	}
+
+	private void FirstEvents()
+	{
+		FirstActivationEvents.Invoke();
+		Invoke("SecondEvents", delayBefore2ndEvent);
+	}
+
+	private void SecondEvents()
+	{
+		SecondActivationEvents.Invoke();
+	}
+
+}
