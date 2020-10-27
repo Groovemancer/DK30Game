af896fd1c4635318b3c686ebbcd49327364dcc00
westfash <westfash@gmail.com>
2020-10-26 21:56:16 -0500

    Camera Prefab Update
    Made a simple camera prefab that sweeps back and forth

---
+++ Assets/Scripts/Activatables/CameraRotate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject secCamera;
+    public float degreesPerSec = 15f;
+    public float sweepTime = 2f;
+    public float timer = 0.00f;
+    public float sweepDir = 1f;
+   
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float rotAmount = sweepDir*degreesPerSec * Time.deltaTime;
+        float curRot = transform.localRotation.eulerAngles.z;
+        if(timer < sweepTime)
+            {
+            secCamera.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, (curRot + rotAmount)));
+        }
+        else
+            {
+                timer = 0.0f;
+                sweepDir = -1*sweepDir;
+            }
+
+
+    }
+}
